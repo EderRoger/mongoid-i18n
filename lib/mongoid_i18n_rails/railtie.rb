9d443e9af05457f21f209d6fc17ad61b97ba95a0
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2014-10-30 15:59:51 -0200

    new version

--- lib/mongoid_i18n_rails/railtie.rb
+++ lib/mongoid_i18n_rails/railtie.rb
@@ -3,13 +3,10 @@ require 'rails'
 module MongoidI18nRails
   class Railtie < ::Rails::Railtie #:nodoc:
     initializer 'mongoid-i18n-rails' do |app|
-      add("rails/locale/mongoid-en.yml")
-      add("rails/locale/mongoid-pt-BR.yml")
-    end
-
-    def self.add(pattern)
-      files = Dir[File.join(File.dirname(__FILE__), '../..', pattern)]
-      I18n.load_path.concat(files)
+      ["rails/locale/mongoid-en.yml", "rails/locale/mongoid-pt-BR.yml"].each do |pattern|
+        files = Dir[File.join(File.dirname(__FILE__), '../..', pattern)]
+        I18n.load_path.concat(files)
+      end
     end
   end
 end
--- lib/mongoid_i18n_rails/version.rb
+++ lib/mongoid_i18n_rails/version.rb
@@ -1,3 +1,3 @@
 module MongoidI18nRails
-  VERSION = "0.0.3"
+  VERSION = "0.0.4"
 end
